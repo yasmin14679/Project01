4e30ce662de21f01fd78ec02315b7889cef6fe9c
Yasmin-Gonçalves <yasmingoncalves.yg2.yg@gmail.com>
2023-01-17 12:07:16 -0300

    Calculadora

---
+++ JavaScript/Calculadora/script.js
@@ -0,0 +1,45 @@
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
+const display = document.querySelector('.calculator__display')
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+
+    const key = e.target
+    const action = key.dataset.action
+    const keyContent = key.textContent
+    const displayedNum = display.textContent
+
+        if (!action) {
+            console.log('number key!')
+        }
+
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+        ) {
+            console.log('operator key!')
+        }
+
+        if (action === 'decimal') {
+            console.log('decimal key!')
+          }
+          
+          if (action === 'clear') {
+            console.log('clear key!')
+          }
+          
+          if (action === 'calculate') {
+            console.log('equal key!')
+          }
+    }
+    if (!action) {
+        if (displayedNum === '0') {
+          display.textContent = keyContent
+        } else {
+            display.textContent = displayedNum + keyContent
+          }
+      }
+})
